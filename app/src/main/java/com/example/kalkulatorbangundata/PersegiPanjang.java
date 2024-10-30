6ee75fa3381b306beb20d56d37ea700484897e5d
RifkyDYanuar <rifkydewanistemga10@gmail.com>
2024-10-30 21:32:02 +0700

    Fiks PersegiPanjang

--- app/src/main/java/com/example/kalkulatorbangundata/PersegiPanjang.java
+++ app/src/main/java/com/example/kalkulatorbangundata/PersegiPanjang.java
@@ -1,10 +1,13 @@
 package com.example.kalkulatorbangundata;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.ActionBar;
@@ -13,10 +16,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.text.DecimalFormat;
+
 public class PersegiPanjang extends AppCompatActivity {
 
-    private EditText txtPanjang, txtLebar;
-    private Button btnPanjang, btnClear;
+    private EditText txtPanjang, txtLebar, txtHasil;
+    private Button btnHitung,  btnClear;
 
     private RadioGroup Grup;
     private RadioButton rdrLuas, rdrKeliling;
@@ -27,13 +32,89 @@ public class PersegiPanjang extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_persegi_panjang);
-
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
 
 
         txtPanjang = findViewById(R.id.panjang);
         txtLebar = findViewById(R.id.lebar);
+        txtHasil = findViewById(R.id.hasil);
+
+        Grup = findViewById(R.id.grup);
+        rdrLuas = findViewById(R.id.luas);
+        rdrKeliling= findViewById(R.id.keliling);
+
+        btnClear = findViewById(R.id.reset);
+        btnHitung = findViewById(R.id.hitung);
+
+        btnHitung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                hitung();
+            }
+        });
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtHasil.setText("");
+                txtLebar.setText("");
+                txtPanjang.setText("");
+                Grup.clearCheck();
+
+            }
+        });
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    public void hitung(){
+        String Panjang = txtPanjang.getText().toString();
+        String Lebar = txtLebar.getText().toString();
+
+        if(Panjang.isEmpty()){
+            Toast.makeText(this, "Panjang harus diisi!", Toast.LENGTH_SHORT).show();
+        } else if (Lebar.isEmpty()) {
+            Toast.makeText(this, "Lebar Harus diisi", Toast.LENGTH_SHORT).show();
+        }
+
+        double panjang = Double.parseDouble(Panjang);
+        double lebar = Double.parseDouble(Lebar);
+        double hasil;
+
+        int Pilihan = Grup.getCheckedRadioButtonId();
+
+        if (Pilihan==rdrLuas.getId()){
+            hasil = panjang * lebar;
+            if (hasil==(int)hasil){
+                txtHasil.setText(String.valueOf((int) hasil));
+            }else {
+                txtHasil.setText(String.valueOf(hasil));
+            }
+
+
+        }else if (Pilihan==rdrKeliling.getId()){
+            hasil = 2 * (panjang + lebar );
+            if (hasil==(int)hasil){
+                txtHasil.setText(String.valueOf((int) hasil));
+            }else {
+                txtHasil.setText(String.valueOf(hasil));
+            }
+
+        }else {
+            Toast.makeText(this, "Pilih perhitungan", Toast.LENGTH_SHORT).show();
+        }
+
+
+
+
 
     }
 }
